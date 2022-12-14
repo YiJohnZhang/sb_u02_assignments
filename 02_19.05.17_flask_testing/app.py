b7addb3fbe657cdd80791440c197cb0a80d02c14
YiJohnZhang <yijohnzhang@gmail.com>
2022-12-13 18:16:12 -0800

    pause `19.05.17`

--- 02_19.05.17_flask_testing/app.py
+++ 02_19.05.17_flask_testing/app.py
@@ -1,5 +1,37 @@
 from boggle import Boggle
-from flask import Flask;
+from flask import Flask, request, render_template;
 
-boggle_game = Boggle()
+boggleGame = Boggle();
+boggleBoard = boggleGame.make_board();
 
+app = Flask(__name__);
+
+@app.route('/')
+def indexView():
+	'''
+		Index route.
+	'''
+	return render_template('boggleGame.html', board=boggleBoard);
+
+@app.route('/', methods=['POST'])
+def checkWordView():
+	'''
+		A route to check the word.
+	'''
+
+	submittedWord = request.form['boggleWord'];
+	result = Boggle.check_valid_word(boggleBoard, submittedWord)
+
+	if(result == "ok"):
+		
+		# if(Boggle.find(boggleBoard, submittedWord)):
+			# check_valid_word() already calls it.
+
+		return {
+			"result": result,
+			"word": submittedWord
+		};
+
+	return {
+		"result": result
+	};
---
+++ 02_19.05.17_flask_testing/static/index.js
@@ -0,0 +1,71 @@
+// Game Session Variables
+let sessionScore = 0;
+let submittedWords = new Set([]);
+
+// Game Application Variables
+let gameplayCount = 0;	//s6, delete
+let gameStarted = false;
+let timer = 60;
+
+const restartGame = () => {
+	
+	// Game Session Variables
+	sessionScore = 0
+	submittedWords = new Set([]);
+
+	// Game Application Variables
+	gameStarted = !gameStarted;
+	timer = 60;
+
+}
+
+const allowUserToPlayGame = () => {
+
+	const submitFormButton = document.getElementById('submitTestWord');
+	submitFormButton.addEventListener('click', (evt) => {
+		
+		evt.preventDefault();
+		// axios post call
+
+		const {result, word} = /*...*/;
+
+		if(word && !submittedWords.has(word)){
+		
+			sessionScore = sessionScore + word.length;
+			submittedWords.add(word);
+
+		}else if (word && submittedWords.has(word)){
+
+			// do something to say word already used.
+		
+		}
+
+	});
+
+}
+
+const setupGame = () => {
+
+	const reStartGameButton = document.getElementById('reStartGame');
+	reStartGameButton.addEventListener('click', (evt) => {
+
+		restartGame();
+
+		if(gameStarted){
+
+			// gameplayCount = gameplayCount + 1;	//s6: delete
+			allowUserToPlayGame();
+			// timer countdown (step5)
+
+		}else{
+
+			submitFormButton.removeEventListener('click');
+			// 
+
+		}
+
+	});
+
+}
+
+document.addEventListener('load', setupGame)
