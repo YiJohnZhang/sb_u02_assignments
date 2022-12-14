f225adcc30dc1296edb317c8ba88466f5437807c
YiJohnZhang <yijohnzhang@gmail.com>
2022-12-13 22:25:22 -0800

    fin `19.05.17`

--- 02_19.05.17_flask_testing/test.py
+++ 02_19.05.17_flask_testing/test.py
@@ -1,10 +1,43 @@
-from unittest import TestCase
-from app import app
-from flask import session
-from boggle import Boggle
-
+from unittest import TestCase;
+from app import app;
+from flask import session;
+from boggle import Boggle;
+import json;
 
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+	def setUp(self):
+		''''''
 
+	def tearDown(self):
+		''''''
+
+	def test_GETIndex(self):
+		with app.test_client() as request:
+			response = request.get('/');
+			html = response.get_data(as_text=True);
+
+			self.assertEqual(response.status_code, 200);
+			self.assertIn('button id=\"reStartGame\"', html);
+
+	def test_POSTIndex(self):
+		with app.test_client() as request:
+			response = request.post('/', {"boggleWord": "afsd"});
+			data = json.loads(response.get_data(as_text=True));
+
+			self.assertEqual(data.result, 'not-on-board');
+	
+	def test_GETSessionScores(self):
+		with app.test_client() as request:
+			response = request.get('/endgame');
+			data = json.loads(response.get_data(as_text=True));
+
+			self.assertEqual(len(data), 0);
+
+	def test_POSTSessionScores(self):
+		with app.test_client() as request:
+			response = request.post('/endgame', {"score": 8});
+			data = json.loads(response.get_data(as_text=True));
+
+			self.assertIn(data[0]['score'], 8);
